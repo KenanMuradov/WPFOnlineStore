e1815bc2c61f8ae2ab4e229e2cc764abd85fd6c0
Mura_mw07@itstep.edu.az <Mura_mw07@itstep.edu.az>
2022-10-31 15:04:45 +0400

    FINAL

--- WPFOnlineStore/Models/ProductItem.cs
+++ WPFOnlineStore/Models/ProductItem.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WPFOnlineStore.Models;
 
-public class ProductItem
+public class ProductItem : INotifyPropertyChanged
 {
 
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+    private uint _count;
+
+    public uint Count
+    {
+        get { return _count; }
+        set 
+        { 
+            _count = value; 
+            NotifyPropertyChanged();
+        }
+    }
+
+
     public Product Product { get; set; }
-    public uint Count { get; set; }
     public double Price { get; set; }
     public double Discount { get; set; }
 
@@ -21,4 +40,5 @@ public class ProductItem
         Price = price;
         Discount = discount;
     }
+
 }
